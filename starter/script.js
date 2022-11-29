2368ebcd1c17c4b40cd4f475b748a676b0575b0d
cherryyeer <yuchenwork57@gmail.com>
2022-11-29 22:40:41 +0000

    set up starter code

---
+++ starter/script.js
@@ -0,0 +1,138 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  var length = prompt("Enter a number to choose the length of your password: (At least 10 characters but no more than 64)")
+  console.log(length)
+
+  var typesLowercase = prompt("Enter Yes or No to choose if your password includes the following character types: 1. Lowercase")
+  console.log(typesLowercase)
+
+  var typesUppercase = prompt("2. Numeric")
+  console.log(typesUppercase)
+
+  var typesSpecial = prompt("3.Special characters")
+  console.log(typesSpecial)
+}
+getPasswordOptions()
+// Function for getting a random element from an array
+function getRandom(arr) {
+  if (typesLowercase == "Yes") {
+
+  }
+
+  if (typesUppercase == "Yes") {
+    
+  }
+
+  if (typesSpecial == "Yes") {
+    
+  }
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
